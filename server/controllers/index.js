e3ef72e37dc00d197541c69429359d0bea5f5e78
bimanathanael95@gmai.com <bimanathanael95@gmail.com>
2020-09-14 11:17:37 +0700

    add below: -add API POST end point /generateNewSalary -generate new salary base on working time, break time, and job title -save and update new salary to table employeeSalaries

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,10 +1,9 @@
-const { EmployeeSalary, Employee, EmployeeAbsence, EmployeeLeave } = require('../models')
+const { EmployeeSalary, Employee, EmployeeAbsence, EmployeeLeave, JobTitle } = require('../models')
 
 class Controller {
   static getSalary (req, res){
     EmployeeSalary.findAll({include: ['Employee']})
     .then( data => {
-      
       return res.status(200).json(data)
     })
   }
@@ -17,23 +16,13 @@ class Controller {
         const tomorrow = new Date(today)
         let day = 0
         while (day < 90) {
-          let breakTime = Math.floor(Math.random() * 90)
-          let year = tomorrow.getFullYear();
-          let month = tomorrow.getMonth()+1;
-          let dt = tomorrow.getDate();
-          let dateFormat
-
-          if (dt < 10) {
-            dt = '0' + dt;
-          }
-          if (month < 10) {
-            month = '0' + month;
-          }
-          dateFormat = year+'-' + month + '-'+dt
-          let newData = {
+          const startTime = tomorrow.setHours(Math.floor(Math.random() * 4) + 8)
+          const endTime = tomorrow.setHours(Math.floor(Math.random() * 4) + 17)
+          const breakTime = Math.floor(Math.random() * 90)
+          const newData = {
             EmployeeId: item.dataValues.id,
-            start_date: dateFormat,
-            end_date: dateFormat,
+            start_date: startTime,
+            end_date: endTime,
             break_time: breakTime,
             createdAt: new Date(),
             updatedAt: new Date(),
@@ -63,22 +52,10 @@ class Controller {
           const leaveType = ["Annual", "Sick", "Unpaid", "Maternity"]
 
           if(randomLeave === 1){
-            let year = tomorrow.getFullYear();
-            let month = tomorrow.getMonth()+1;
-            let dt = tomorrow.getDate();
-            let dateFormat
-
-            if (dt < 10) {
-              dt = '0' + dt;
-            }
-            if (month < 10) {
-              month = '0' + month;
-            }
-            dateFormat = year+'-' + month + '-'+dt
             let newData = {
               EmployeeId: item.dataValues.id,
-              start_date: dateFormat,
-              end_date: dateFormat,
+              start_date: tomorrow,
+              end_date: tomorrow,
               type: item.dataValues.gender === "F" ? leaveType[randomFemale] : leaveType[randomMale],
               createdAt: new Date(),
               updatedAt: new Date(),
@@ -90,6 +67,84 @@ class Controller {
         }
       });
       return res.status(200).json({message: "done random employee leave"})
+    })
+  }
+
+  static generateNewSalary (req, res){
+    let currSalary = 0
+    let totalPercentage = req.body.pandemic == "true" ?  -1 : 0
+    Employee.findAll()
+    .then( data => {
+      data.forEach(item => {
+  
+        EmployeeSalary.findOne({where: { EmployeeId: item.dataValues.id}})
+        .then( salaryData => {
+          currSalary = salaryData.dataValues.salary
+          EmployeeAbsence.findAll({where: {EmployeeId: item.dataValues.id}})
+          .then( absenceData => {
+            let totalDay = 0
+            let totalHours = 0
+            let totalBreakTime = 0
+            absenceData.forEach( oneDay => {
+              totalDay += 1
+              let start = oneDay.dataValues.start_date.getTime()
+              let end = oneDay.dataValues.end_date.getTime()
+              totalHours += (end - start) /3600 /1000
+              totalBreakTime += oneDay.dataValues.break_time
+            })
+            let avgWorkingHours = totalHours/totalDay
+            let avgBreaktime = totalBreakTime/totalDay
+  
+            if(avgWorkingHours > 10 ) totalPercentage += 5
+            else if (avgWorkingHours <= 10 && avgWorkingHours > 8) totalPercentage += 4
+            else if (avgWorkingHours <= 8 && avgWorkingHours > 7) totalPercentage += 2.5
+            else if (avgWorkingHours <= 7 && avgWorkingHours > 5) totalPercentage += 0.5
+            
+  
+            if(avgBreaktime > 10 ) totalPercentage -= 1
+            else totalPercentage += 1.5
+  
+  
+            JobTitle.findOne({where: {EmployeeId: item.dataValues.id}})
+            .then( dataTitle => {
+              switch (dataTitle.dataValues.title) {
+                case "Staff":
+                  totalPercentage += 1
+                  break;
+                case "Senior Engineer":
+                  totalPercentage += 3
+                  break;
+                case "Engineer":
+                  totalPercentage += 2
+                  break;
+                case "Assistant Engineer":
+                  totalPercentage += 2.5
+                  break;
+                case "Technical Leader":
+                  totalPercentage += 4
+                  break;
+                default:
+                  currSalary += 1000
+                  break;
+              }
+              let endTime = new Date()
+              endTime.setDate(endTime.getDate() + 90)
+              
+              currSalary = currSalary + ((currSalary * totalPercentage) /100)
+              let newData = {
+                EmployeeId: item.dataValues.id,
+                start_date: new Date(),
+                end_date: endTime,
+                salary: Math.floor(currSalary),
+                createdAt: new Date(),
+                updatedAt: new Date(),
+              }
+              EmployeeSalary.update(newData, {where: {EmployeeId: item.dataValues.id}})
+            })
+          })
+        })
+      });
+      return res.status(200).json({message: "done generate new salary"})
     })
   }
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,9 @@ const router = require('express').Router()
 const controller = require('../controllers')
 
 router.get('/salary', controller.getSalary)
-router.get('/randomAbsence', controller.randomAbsence)
-router.get('/randomLeaves', controller.randomLeaves)
+router.post('/randomAbsence', controller.randomAbsence)
+router.post('/randomLeaves', controller.randomLeaves)
+router.post('/generateNewSalary', controller.generateNewSalary)
+
 
 module.exports = router
